4f39dde2671b43ac746c6d0570c934e70239dbf1
Carlos Lantigua <clantigua954@gmail.com>
2018-08-26 19:06:57 -0400

    Added delete function and FlipMove animation

--- src/TodoItems.js
+++ src/TodoItems.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
+import FlipMove from 'react-flip-move';
 
 // this adds all entries to ul as li elements
 class TodoItems extends Component {
+  constructor(props) {
+    super(props);
+    this.createTasks = this.createTasks.bind(this);
+  }
+
   createTasks(item) {
-    return <li key={item.key}>{item.text}</li>
+    return <li onClick={() => this.delete(item.key)}
+      key={item.key}>{item.text}</li>
+  }
+
+  delete(key) {
+    this.props.delete(key);
   }
 
   render() {
@@ -12,7 +23,9 @@ class TodoItems extends Component {
 
     return (
       <ul className="theList">
-        {listItems}
+        <FlipMove duration={250} easing="ease-out">
+          {listItems}
+        </FlipMove>
       </ul>
     );
   }
